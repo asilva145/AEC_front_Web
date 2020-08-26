460172f81abb34d499879d11a5ceab05f90efbed
asilva145 <andrea.fabricia@gmail.com>
2020-08-26 01:07:43 -0500

    commit

---
+++ becflix - Copy (2)/src/components/TvShow/TvShow.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+
+import showImage from '../../img/logo.png';
+
+let image ={
+    src:showImage,
+    alt:'les invisibles',
+    title: 'Les invicibles'
+}
+
+export class TvShow extends React.Component{
+render (){
+    return(
+        <col xs lg="2">
+            <img src={image.src}
+            alt={image.alt}
+            title={image.title}
+            className='img-fluid'
+            
+            />
+        </col>
+    );
+}
+
+}
